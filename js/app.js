21b51e6010735baf4e1b51f9d0f12369e0c5bf93
Michael Pace <michael.g.pace@gmail.com>
2021-12-07 20:45:26 -0600

    added if conditions in question 1, in  app js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 'use strict';
 console.log('app.js file is connected');
+let userName;
+while(!userName){
+  userName = prompt('Hello there!  You\'ve reached Mike Pace\'s About Me page.  What is your name?');
+}
+alert (userName + ', I hope you\'re ready for the most important questionnaire of your life!');
 
-let userName = prompt('Hello there!  You\'ve reached Mike Pace\'s About Me page.  What is your name?');
 
-alert (userName + ' I hope you\'re ready for the most important questionnaire of your life!');
+let answer1 = prompt('Was I born in Japan?').toLowerCase();
 
-prompt('Was I born in Japan?') {
-  if 
+if (answer1 === 'yes' || answer1 === 'y' || answer1 === 'ya' || answer1 === 'yeah') {
+  alert('Wrong!');
+}
+
+if (answer1 === 'no' || answer1 === 'n' || answer1 === 'na' || answer1 === 'nah') {
+  alert('How did you know?');
 }
