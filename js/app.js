5dde57e0e5f84676f8edd38092163b99e57d3abb
Michael Pace <michael.g.pace@gmail.com>
2021-12-07 20:01:17 -0600

    started js, added bio to index

--- js/app.js
+++ js/app.js
@@ -2,3 +2,9 @@
 console.log('app.js file is connected');
 
 let userName = prompt('Hello there!  You\'ve reached Mike Pace\'s About Me page.  What is your name?');
+
+alert (userName + ' I hope you\'re ready for the most important questionnaire of your life!');
+
+prompt('Was I born in Japan?') {
+  if 
+}
