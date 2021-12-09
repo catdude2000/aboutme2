4ac1120eb004af03ded37e6e94b01807495010a6
Michael Pace <michael.g.pace@gmail.com>
2021-12-08 18:12:02 -0600

    added q2 and answers

--- js/app.js
+++ js/app.js
@@ -10,9 +10,24 @@ alert (userName + ', I hope you\'re ready for the most important questionnaire o
 let answer1 = prompt('Was I born in Japan?').toLowerCase();
 
 if (answer1 === 'yes' || answer1 === 'y' || answer1 === 'ya' || answer1 === 'yeah') {
+  console.log('Wrong');
   alert('Wrong!');
 }
 
 if (answer1 === 'no' || answer1 === 'n' || answer1 === 'na' || answer1 === 'nah') {
+  console.log('Correct');
   alert('How did you know?');
 }
+
+let answer2 = prompt('Am I in the witness protection program?').toLowerCase();
+
+if (answer2 === 'yes' || answer2 === 'y' || answer2 === 'ya' || answer2 === 'yeah') {
+  console.log('Wrong');
+  alert('nu-uh!!');
+}
+
+if (answer2 === 'no' || answer2 === 'n' || answer2 === 'na' || answer2 === 'nah') {
+  console.log('Correct');
+  alert('Stop reading my mind!');
+}
+
