654a7a1b6ea0fdccf449fdf19cbaef9102b4276f
catdude2000 <michael.g.pace@gmail.com>
2021-12-14 15:08:41 -0600

    rewrote q7

--- js/otrojs.js
+++ js/otrojs.js
@@ -2,41 +2,20 @@
 function questionSev(){
   let answer7 = prompt('Can you name one of my brothers?').toLowerCase();
   console.log('user input', answer7);
-  // for(let i = 0; i <answers.length; i++);
   let tries2 = 0;
   let answers = ['david', 'tom', 'chris'];
   while (tries2 < 5) {
     for(let i = 0; i <answers.length; i++){ 
-    if(answer7 === answers[i]) {
-      alert('youre right');
-      tries2 = tries2 + 1;
-    } else{
-      alert('your wrong');
-      tries2 = tries2 + 1;
+      if(answer7 === answers[i]) {
+        alert('That\'s correct!');
+      } else{
+        alert('Sorry, try again.');
+        tries2 = tries2 + 1;
+      }
     }
-    }
-    alert('you got x right');
   }
-  // (answer7 !== answers[i] && tries2 < 5) {
-    // for(let i = 0; i <answers.length; i++);
-  //   tries2 = tries2 + 1;
-  //   answer7 = prompt('Sorry but no, try again.').toLowerCase();
-  // }
-  
-  // while (answer7 == answers[i]){
-  //   alert('You are correct!');
-  //   points += 1;
-  //   break;
-  }
-  while (tries2 === 5 || answer7 === answers[i]) {
-    alert('Their names are David, Tom and Chris');
-    console.log(userName + ' has ' + points + ' points');
-    break;
-  }
-  console.log('tries', tries2);
-}
 
-questionSev();
+  questionSev();
 
 console.log('points', points);
 
