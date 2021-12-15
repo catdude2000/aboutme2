6efe748bf7bf7374563eede2a12328876dc8d715
Michael Pace <michael.g.pace@gmail.com>
2021-12-14 19:08:51 -0600

    formatted

--- js/app.js
+++ js/app.js
@@ -1,23 +1,17 @@
 'use strict';
 console.log('app.js file is connected');
-
 let points = 0;
-
 let userName = prompt('Hello there!  You\'ve reached Mike Pace\'s About Me page.  What is your name?');
 while (!userName) {
   userName = prompt ('Enter your name, please.');
 }
-
 alert(userName + ', I hope you\'re ready for the most important questionnaire of your life!');
-
 function questionOne(){
   let answer1 = prompt('Was I born in Japan?').toLowerCase();
-
   if (answer1 === 'yes' || answer1 === 'y' || answer1 === 'ya' || answer1 === 'yeah') {
     console.log('Wrong');
     alert('Wrong!');
   }
-
   if (answer1 === 'no' || answer1 === 'n' || answer1 === 'na' || answer1 === 'nah') {
     console.log('Correct');
     alert('How did you know?');
@@ -25,19 +19,14 @@ function questionOne(){
     console.log(userName + ' has ' + points + ' points');
   }
 }
-
 questionOne();
-
 console.log('points', points);
-
 function questionTwo(){
   let answer2 = prompt('Am I in the witness protection program?').toLowerCase();
-
   if (answer2 === 'yes' || answer2 === 'y' || answer2 === 'ya' || answer2 === 'yeah') {
     console.log('Wrong');
     alert('nu-uh!!');
   }
-
   if (answer2 === 'no' || answer2 === 'n' || answer2 === 'na' || answer2 === 'nah') {
     console.log('Correct');
     alert('Stop reading my mind!');
@@ -45,40 +34,29 @@ function questionTwo(){
     console.log(userName + ' has ' + points + ' points');
   }
 }
-
 questionTwo();
-
 console.log('points', points);
-
 function questionThree(){
-
   let answer3 = prompt('Is my net handle Catdude?').toLowerCase();
-
   if (answer3 === 'yes' || answer3 === 'y' || answer3 === 'ya' || answer3 === 'yeah') {
     console.log('Correct');
     alert('Have you been stalking me?');
     points += 1;
     console.log(userName + ' has ' + points + ' points');
   }
-
   if (answer3 === 'no' || answer3 === 'n' || answer3 === 'na' || answer3 === 'nah') {
     console.log('Wrong');
     alert('Shame, shame.');
   }
 }
-
 console.log('points', points);
-
 questionThree();
-
 function questionFour(){
   let answer4 = prompt('Do I own a cat?').toLowerCase();
-
   if (answer4 === 'yes' || answer4 === 'y' || answer4 === 'ya' || answer4 === 'yeah') {
     console.log('Wrong');
     alert('No, why would you say that!?');
   }
-
   if (answer4 === 'no' || answer4 === 'n' || answer4 === 'na' || answer4 === 'nah') {
     console.log('Correct');
     alert('Way to rub it in...');
@@ -86,43 +64,31 @@ function questionFour(){
     console.log(userName + ' has ' + points + ' points');
   }
 }
-
 questionFour();
-
 console.log('points', points);
-
 function questionFive(){
   let answer5 = prompt('Am I allergic to cats?').toLowerCase();
-
   if (answer5 === 'yes' || answer5 === 'y' || answer5 === 'ya' || answer5 === 'yeah') {
     console.log('Correct');
     points += 1;
     alert('Allergies, also known as Mike\'s-bane.');
     console.log(userName + ' has ' + points + ' points');
   }
-
   if (answer5 === 'no' || answer5 === 'n' || answer5 === 'na' || answer5 === 'nah') {
     console.log('Wrong');
     alert('I wish you were right.');
-
   }
 }
-
 questionFive();
-
 console.log('points', points);
-
 function questionSix() {
   let answer6 = prompt ('How old am I?');
-
   let tries = 0;
-
   while (answer6 < '31' && tries < 3) {
     console.log(answer6);
     tries = tries + 1;
     answer6 = prompt ('Try a little higher.');
   }
-
   while (answer6 > '31' && tries < 3) {
     console.log(answer6);
     tries = tries + 1;
@@ -133,7 +99,6 @@ function questionSix() {
     tries = tries + 1;
     answer6 = prompt ('Try a little higher.');
   }
-
   while (answer6 > '31' && tries < 3) {
     console.log(answer6);
     tries = tries + 1;
@@ -153,11 +118,8 @@ function questionSix() {
   }
   console.log('tries', tries);
 }
-
 questionSix();
-
 console.log('points', points);
-
 function questionSev(){
   let tries2 = 0;
   let answers = ['david', 'tom', 'chris'];
@@ -180,9 +142,7 @@ function questionSev(){
     tries2 = tries2 + 1;
   }
 }
-
 questionSev();
 alert('Their names are David, Tom, and Chris');
 console.log('points', points);
-
 alert(userName + ', you got ' + points + ' out of 7!');
